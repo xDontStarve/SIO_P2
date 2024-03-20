92df7cea576d09051624df7addff4565d5ae088d
Jialiang Chen <jialiang.chen@estudiants.urv.cat>
2024-03-20 16:29:24 +0100

    Main: script added

--- P1/P2_Phase1.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import mysql.connector as db
-
-
-if __name__ == "__main__":
-    db.connect()
---
+++ P2/P2_Phase1.py
@@ -0,0 +1,5 @@
+import mysql.connector as db
+
+
+if __name__ == "__main__":
+    db.connect(user="root", password="root", host="127.0.0.1", database="project01")
