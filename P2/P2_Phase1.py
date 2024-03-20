2263ab81eb9cce2a3ccbef2833e79a191e73cfb1
Kovács Benedek <kbeny40@gmail.com>
2024-03-20 16:39:48 +0100

    Initial commit

--- P2/P2_Phase1.py
+++ P2/P2_Phase1.py
@@ -2,4 +2,4 @@ import mysql.connector as db
 
 
 if __name__ == "__main__":
-    db.connect(user="root", password="root", host="127.0.0.1", database="project01")
+    db.connect(user="root", password="Kocsi0112", host="127.0.0.1", database="project01")
